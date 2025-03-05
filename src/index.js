9cfc65184f94a3e1818aa7db866e75f4f4b7290e
Joshua Cilufo <joshua.cilufo@student.swic.edu>
2025-03-04 20:54:01 -0600

    Add arrow function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-var x = 3;
+// Functions Inside Variables and Arrow Functions
+const backpackFullOfAmmo = [
+    { item: "bullets", quantity: 10, priceEa: 0.5 },
+    { item: "shotgun shells", quantity: 4, priceEa: 0.5 },
+    { item: "rockets", quantity: 1, priceEa: 5.0 },
+    { item: "energy cell units", quantity: 20, priceEa: 0.2 },
+]
 
-console.log(x);
+const calcOrderCost = (order) =>  order.quantity * order.priceEa
