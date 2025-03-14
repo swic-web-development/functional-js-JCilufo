f07fcf7d7112c97a2a474ee9ca08f31747bf6750
Joshua Cilufo <joshua.cilufo@student.swic.edu>
2025-03-13 23:40:16 -0500

    Add coding challenge 2

--- src/arrays-practice.js
+++ src/arrays-practice.js
@@ -95,3 +95,59 @@ const totalValue = backpackFullOfAmmo.reduce((accumulator, currentItem) => {
 console.log(`Total Value: ${totalValue}`);
 
 // Coding challenge 2
+const monsters = [
+    // Deals 10-60 damage per bite
+    { name: 'Cacodemon', health: 400, damage: 35 },
+    
+    // Deals 15-75 damage per claw/fireball
+    { name: 'Baron of Hell', health: 1000, damage: 45 },
+    
+    // Deals 20-200 damage per rocket
+    { name: 'Cyberdemon', health: 4000, damage: 125 },
+    
+    // Deals 5-50 damage per bite
+    { name: 'Hell Knight', health: 800, damage: 25 },
+    
+    // Deals 10-100 damage per bite
+    { name: 'Imp', health: 200, damage: 20 },
+    
+    // Deals 5-25 damage per bite
+    { name: 'Lost Soul', health: 100, damage: 10 },
+    
+    // Deals 5-50 damage per bite
+    { name: 'Pinky', health: 300, damage: 15 },
+    
+    // Deals 10-100 damage per bite
+    { name: 'Revenant', health: 500, damage: 30 },
+    
+    // Deals 5-50 damage per bite
+    { name: 'Spectre', health: 200, damage: 20 },
+    
+    // Deals 5-50 damage per bite
+    { name: 'Spider Mastermind', health: 2000, damage: 50 },
+    
+    // Deals 5-50 damage per bite
+    { name: 'Vile', health: 1000, damage: 40 },
+    
+    // Deals 5-50 damage per bite
+    { name: 'Zombie', health: 100, damage: 5 },
+    
+    // Deals 5-50 damage per bite
+    { name: 'Zombieman', health: 200, damage: 10 },
+  ];
+
+// I used the map method to create an array that only displays the names of each monster.
+  const monsterNames = monsters.map(monster => monster.name);
+
+  // I used the filter method to create an array that only displays the monster that have more than 150 health.
+  const healthyMonsters = monsters.filter(monster => monster.health > 150);
+
+  // I used the reduce method to calculate the total health of all the monsters.
+  const totalHealth = monsters.reduce((accumulator, monster) => {
+    return accumulator + monster.health;
+  }, 0);
+
+  // I used the sort method to sort the monsters by damage in descending order.
+  const sortedMonsters = monsters.sort((a,b) => b.damage - a.damage);
+
+  console.log(monsterNames, healthyMonsters, totalHealth, sortedMonsters);
